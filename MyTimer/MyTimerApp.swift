40f2186b450a117fe83e6cce3bb22907cb7fc4de
RADISH-miyazaki <y.hidaka.kobe@gmail.com>
2023-11-18 12:29:11 +0900

    Initial Commit

---
+++ MyTimer/MyTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyTimerApp.swift
+//  MyTimer
+//
+//  Created by Hidaka Yoshiki on 2023/11/18.
+//
+
+import SwiftUI
+
+@main
+struct MyTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
